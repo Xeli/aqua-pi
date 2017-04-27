f1f5185843d5b84a112ebf72c9a620261192df76
richard@xeli.eu <richard@xelification.com>
2017-04-27 13:25:19 +0200

    catch pigpio exception when checking what the pwm value on the rpi is

--- src/main/scala/eu/xeli/aquariumPI/gpio/Pwm.scala
+++ src/main/scala/eu/xeli/aquariumPI/gpio/Pwm.scala
@@ -2,6 +2,7 @@ package eu.xeli.aquariumPI.gpio
 
 import eu.xeli.aquariumPI.Server
 import jpigpio.JPigpio
+import jpigpio.PigpioException
 
 class Pwm(server: Server, pin: Int) {
   val pigpio = Pigpio.getInstance(server)
@@ -16,7 +17,11 @@ class Pwm(server: Server, pin: Int) {
   }
 
   def getValue(): Double = {
-    val dutycycle = pigpio.getPWMDutycycle(pin)
-    (dutycycle / 250.0) * 100.0
+    try {
+      val dutycycle = pigpio.getPWMDutycycle(pin)
+      (dutycycle / 250.0) * 100.0
+    } catch {
+      case pie: PigpioException => 0.0
+    }
   }
 }
