f68b017a6eecaad4caedbd2176b662cb1d358c46
richard@xeli.eu <richard@xelification.com>
2017-06-18 20:00:48 +0200

    Relay now extends Output

--- src/main/scala/eu/xeli/aquariumPI/gpio/Relay.scala
+++ src/main/scala/eu/xeli/aquariumPI/gpio/Relay.scala
@@ -1,11 +1,16 @@
 package eu.xeli.aquariumPI.gpio
 
+import eu.xeli.aquariumPI.Output
 import eu.xeli.aquariumPI.gpio._
 import eu.xeli.aquariumPI.Server
 import eu.xeli.jpigpio._
 
-class Relay(pigpio: JPigpio, pin: Int, hasInverseHardware: Boolean = false) {
+class Relay(pigpio: JPigpio, pin: Int, hasInverseHardware: Boolean = false) extends Output {
   pigpio.gpioSetMode(pin, JPigpio.PI_OUTPUT)
+
+  def enable() = set(true)
+  def disable() = set(false)
+  def setValue(value: Double) = set(value > 0.5)
 
   private def setRelay(state: Boolean) {
     if (state) {
@@ -13,6 +18,15 @@ class Relay(pigpio: JPigpio, pin: Int, hasInverseHardware: Boolean = false) {
     } else {
       pigpio.gpioWrite(pin, JPigpio.PI_HIGH)
     }
+  }
+
+  def getValue(): Double = {
+    var isHigh = pigpio.gpioRead(pin)
+    if (hasInverseHardware) {
+      isHigh = !isHigh
+    }
+
+    if (isHigh) 1.0 else 0.0
   }
 
   def set(state: Boolean) {
@@ -23,11 +37,4 @@ class Relay(pigpio: JPigpio, pin: Int, hasInverseHardware: Boolean = false) {
     }
   }
 
-  def enable() {
-    set(true)
-  }
-
-  def disable() {
-    set(false)
-  }
 }
