f9363c2dca1565d88b141e79f8d27ebfae909822
richard@xeli.eu <richard@xelification.com>
2017-06-22 09:15:52 +0200

    add logging to Relay

--- src/main/scala/eu/xeli/aquariumPI/gpio/Relay.scala
+++ src/main/scala/eu/xeli/aquariumPI/gpio/Relay.scala
@@ -5,7 +5,12 @@ import eu.xeli.aquariumPI.gpio._
 import eu.xeli.aquariumPI.Server
 import eu.xeli.jpigpio._
 
+import com.typesafe.scalalogging.Logger
+
 class Relay(pigpio: JPigpio, pin: Int, hasInverseHardware: Boolean = false) extends Output {
+  val logger = Logger[Relay]
+
+  logger.debug(s"Initializing relay on pin $pin")
   pigpio.gpioSetMode(pin, JPigpio.PI_OUTPUT)
 
   def enable() = set(true)
@@ -13,6 +18,7 @@ class Relay(pigpio: JPigpio, pin: Int, hasInverseHardware: Boolean = false) exte
   def setValue(value: Double) = set(value > 0.5)
 
   private def setRelay(state: Boolean) {
+    logger.debug(s"Setting relay on pin $pin to " + (if (state) "low" else "high"))
     if (state) {
       pigpio.gpioWrite(pin, JPigpio.PI_LOW)
     } else {
