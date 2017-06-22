05e46719d4d17253494f284cc9aecc6c5b97f92a
richard@xeli.eu <richard@xelification.com>
2017-06-22 09:04:15 +0200

    more natural order of function in the file

--- src/main/scala/eu/xeli/aquariumPI/Controllee.scala
+++ src/main/scala/eu/xeli/aquariumPI/Controllee.scala
@@ -3,10 +3,9 @@ package eu.xeli.aquariumPI
 abstract class Controllee(val priority: Int) extends Ordered[Controllee] {
   var activated = true
 
+  def getValue(): Double
+  def getFrequency(): Int
   def compare(a: Controllee): Int = {
     priority.compare(a.priority)
   }
-
-  def getValue(): Double
-  def getFrequency(): Int
 }
